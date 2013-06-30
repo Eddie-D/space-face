b2c7d9188634d83d6133a5939252dcc0ede61bd6
Eddie Dombrowski <Eddie@Eddies-MacBook-Pro.local>
2013-06-29 19:47:29 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpaceFace::Application.config.secret_token = 'd9aeae27aebd1fff824bc3b9989ac26434d70b6a159540b44f8329ebb5423426a8edcbb4e1c7a1a711d679d09e231ab801d7589451176c6c6640f446ecaae511'
